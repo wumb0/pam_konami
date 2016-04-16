f99ff272f0e20f0396a1a65190010094c9c1c031
jgeigerm <jmg2967@rit.edu>
2016-04-16 15:39:18 -0400

    added comments

--- pam_konami.c
+++ pam_konami.c
@@ -6,17 +6,14 @@
 #include <sys/param.h>
 #include <pwd.h>
 
-/* expected hook */
 PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) {
-	return PAM_SUCCESS;
+    return PAM_SUCCESS;
 }
 
 PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv) {
-	printf("Acct mgmt\n");
-	return PAM_SUCCESS;
+    return PAM_SUCCESS;
 }
 
-/* expected hook, this is where custom stuff happens */
 PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) {
     char *prompt = "";
     char seq[] = {0x1b, 0x5b, 0x41, 0x1b, 0x5b, 0x41, 0x1b, 0x5b, 0x42, 0x1b, 0x5b, 0x42, 0x1b, 0x5b, 0x44, 0x1b, 0x5b, 0x43, 0x1b, 0x5b, 0x44, 0x1b, 0x5b, 0x43, 0x62, 0x61};
@@ -28,16 +25,22 @@ PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, cons
     struct passwd *pwd;
     struct pam_response *resp;
     resp = NULL;
+
+    // make sure user exists
     if ((retval = pam_get_user(pamh, &user, NULL)) != PAM_SUCCESS)
         return (retval);
     if ((pwd = getpwnam(user)) == NULL)
         return (PAM_USER_UNKNOWN);
+
+    //setup prompt
     retval = pam_get_item(pamh, PAM_CONV, (const void **)&conv);
     if (retval != PAM_SUCCESS)
         return (PAM_SYSTEM_ERR);
     msg.msg_style = PAM_PROMPT_ECHO_OFF;
     msg.msg = prompt;
     msgp = &msg;
+
+    //get password
     retval = (*conv->conv)(1, &msgp, &resp, conv->appdata_ptr);
     if (resp != NULL) {
         if (retval == PAM_SUCCESS)
@@ -46,6 +49,8 @@ PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, cons
             free(resp->resp);
         free(resp);
     }
+
+    //check code
     for (int i = 0; i < sizeof(seq); ++i){
         if (password[i] != seq[i]){
             return PAM_AUTH_ERR;
